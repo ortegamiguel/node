a93ec39d1877c763f328c7e6d2584e1b870ed9cd
Miguel Ortega <omiguel1992@gmail.com>
2020-04-06 18:27:10 -0400

    http servevr rutas

--- section7/idx.js
+++ section7/idx.js
@@ -1,12 +1,22 @@
 const http = require('http');
 const fs = require('fs');
 
-const html = fs.readFileSync('./index.html');
+const home = fs.readFileSync('./index.html');
+const about = fs.readFileSync('./about.html');
 
 http
     .createServer((request, response) => {
-        response.writeHead(200, {'Content-type': 'text/html'})
-        response.write(html);
+        const {url} = request;
+        if(url === '/'){
+            response.writeHead(200, {'Content-type': 'text/html'})
+            response.write(home);
+        } else if(url === '/about') {
+            response.writeHead(200, {'Content-type': 'text/html'})
+            response.write(about);
+        } else {
+            response.writeHead(404, {'Content-type': 'text/html'})
+            response.write('Page not found!!')
+        }
         response.end();
     })
     .listen(8080);
